2eaaaae8ae0f984b3510287129f131500f06a830
San Lee <allen000525@gmail.com>
2022-05-01 22:47:27 -0700

    now downloads websites

--- src/Crawler/Crawler.java
+++ src/Crawler/Crawler.java
@@ -1,8 +1,11 @@
 package Crawler;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayDeque;
 
+import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -17,24 +20,54 @@ public class Crawler {
 	public Crawler(int depth) {
 		this.MAX_DEPTH = depth;
 	}
+	// create html file named as paramterized filename
+	private String createTitle(String filename) {
+		StringBuilder title = new StringBuilder(dest);
+		if(filename.contains("http://")) {
+			title.append(filename.replace("http://", "")); 
+		}
+		else if(filename.contains("https://")) {
+			title.append(filename.replace("https://", "")); 
+		}
+		else {
+			return "";
+		}
+		title.append(".html");
+		
+		return title.toString();
+	}
 	
 	public void crawl() {
 		// add seed link to frontier, currently just manual
 		LinkNode seed = new LinkNode("http://alaska.edu");
 		frontier.add(seed);
-		
 		// run as long as frontier has some links to crawl
-		// for(int i = 0; i < 2; i++){
-		while(!frontier.isEmpty()) {
+		for(int i = 0; i < 1; i++){
+		// while(!frontier.isEmpty()) {
 			LinkNode curr = frontier.poll();
 			try {
+				// fetch pages from URL
 				Document doc = Jsoup.connect(curr.getLink()).get();
-				// somehow download html files here?
+				Connection.Response html = Jsoup.connect(curr.getLink()).execute();
+				
 				Elements links = doc.select("a[href]");
-				// to see what websites are visited
+				
+				String title = createTitle(curr.getLink());
+				if(title.equals("")) {
+					// current link is not appropriate URL
+					continue;
+				}
+				String htmlText = html.body();
+				BufferedWriter writer = new BufferedWriter(new FileWriter(title));
+				writer.write(htmlText);
+				writer.close();
+				// to see what pages are visited
 				// System.out.println(curr.getLink());
+				// System.out.println(html.body());
 				for(Element e: links) {
-					if(curr.getDepth() >= MAX_DEPTH) {
+					// if current page reaches max depth
+					//	or link is not .edu format
+					if(curr.getDepth() >= MAX_DEPTH || !curr.getLink().contains(".edu")) {
 						continue;
 					}
 					LinkNode next = new LinkNode(e.attr("abs:href"), curr.getDepth() + 1);
@@ -42,7 +75,7 @@ public class Crawler {
 				}
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
-				e.printStackTrace();
+				System.out.println(curr.getLink() + "is not a proper URL.");
 			}
 		}
 	}
@@ -50,4 +83,5 @@ public class Crawler {
 	// any data structure implementing Queue should work
 	private ArrayDeque<LinkNode> frontier;
 	private int MAX_DEPTH = 5;
+	private final String dest = "src/pages/";
 }
