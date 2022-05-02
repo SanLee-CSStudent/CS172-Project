8b50dc194666dd550067c530c130337d0eea13dc
San Lee <allen000525@gmail.com>
2022-05-01 23:38:03 -0700

    Normalization for URL

--- src/Crawler/Crawler.java
+++ src/Crawler/Crawler.java
@@ -24,7 +24,16 @@ public class Crawler {
 	}
 	
 	public Crawler(int depth) {
+		this.frontier = new ArrayDeque<LinkNode>();
+		this.visited = new HashSet<String>();
 		this.MAX_DEPTH = depth;
+	}
+	
+	public Crawler(int depth, int numPages) {
+		this.frontier = new ArrayDeque<LinkNode>();
+		this.visited = new HashSet<String>();
+		this.MAX_DEPTH = depth;
+		this.MAX_NUM_PAGES = numPages;
 	}
 	
 	// load names of pages that are downloaded to check redundant pages
@@ -39,17 +48,26 @@ public class Crawler {
 	}
 	
 	// create html file named as paramterized filename
+	// normalization took place: remove http/https protocol and redundant www.
 	private String createTitle(String filename) {
 		StringBuilder title = new StringBuilder(dest);
 		StringBuilder link = new StringBuilder();
 		if(filename.contains("http://")) {
-			link.append(filename.replace("http://", "")); 
+			filename = filename.replace("http://", ""); 
 		}
 		else if(filename.contains("https://")) {
-			link.append(filename.replace("https://", "")); 
+			filename = filename.replace("https://", ""); 
 		}
 		else {
 			return "";
+		}
+		if(filename.contains("www.")) {
+			filename = filename.replace("www.", "");
+		}
+		
+		link.append(filename);
+		if(filename.endsWith("/")) {
+			link.deleteCharAt(filename.length() - 1);
 		}
 		link.append(".html");
 		title.append(link.toString().replace("/", "-"));
@@ -59,12 +77,21 @@ public class Crawler {
 	
 	public void crawl() {
 		// add seed link to frontier, currently just manual
+		int numPages = 0;
+		String seedFile = "src/seed.txt";
+		
 		LinkNode seed = new LinkNode("http://alaska.edu");
 		frontier.add(seed);
+		numPages++;
 		// run as long as frontier has some links to crawl
 		// for(int i = 0; i < 15; i++){
 		while(!frontier.isEmpty()) {
+			if(numPages > MAX_NUM_PAGES) {
+				// crawler reached page number limit
+				return;
+			}
 			LinkNode curr = frontier.poll();
+			numPages++;
 			try {
 				// if the current page is already visited, skip it
 				if(visited.contains(curr.getLink())) {
@@ -79,7 +106,7 @@ public class Crawler {
 				
 				String title = createTitle(curr.getLink());
 				// check title
-				// System.out.println("Title: " + title);
+				System.out.println("Title: " + title);
 				
 				if(title.equals("")) {
 					// current link is not appropriate URL missing http:// or https://
@@ -115,6 +142,7 @@ public class Crawler {
 	// any data structure implementing Queue should work
 	private ArrayDeque<LinkNode> frontier;
 	private int MAX_DEPTH = 5;
+	private int MAX_NUM_PAGES = 100;
 	private final String dest = "src/pages/";
 	private HashSet<String> visited;
 }
