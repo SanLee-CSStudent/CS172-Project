1d5380ddef4369aa221142f70f42d6416ecb3a28
San Lee <allen000525@gmail.com>
2022-05-03 16:49:34 -0700

    Added normalization to the links

--- src/Crawler/Crawler.java
+++ src/Crawler/Crawler.java
@@ -5,8 +5,11 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.util.ArrayDeque;
 import java.util.HashSet;
+import java.util.Scanner;
+import java.util.concurrent.ArrayBlockingQueue;
 
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
@@ -17,72 +20,81 @@ import org.jsoup.select.Elements;
 public class Crawler {
 	
 	public Crawler() {
-		this.frontier = new ArrayDeque<LinkNode>();
+		this.frontier = new ArrayDeque<LinkNode>(MAX_NUM_PAGES);
 		this.visited = new HashSet<String>();
 		
 		// loadExistingPages();
 	}
 	
 	public Crawler(int depth) {
-		this.frontier = new ArrayDeque<LinkNode>();
+		this.frontier = new ArrayDeque<LinkNode>(MAX_NUM_PAGES);
 		this.visited = new HashSet<String>();
 		this.MAX_DEPTH = depth;
 	}
 	
 	public Crawler(int depth, int numPages) {
-		this.frontier = new ArrayDeque<LinkNode>();
+		this.frontier = new ArrayDeque<LinkNode>(MAX_NUM_PAGES);
 		this.visited = new HashSet<String>();
 		this.MAX_DEPTH = depth;
 		this.MAX_NUM_PAGES = numPages;
 	}
 	
-	// load names of pages that are downloaded to check redundant pages
-	// not precise due to the presence of skipping of "www." in URL
-	private void loadExistingPages() {
-		File pages = new File(dest);
-		File[] list = pages.listFiles();
-		
-		for(int i = 0; i < list.length; i++) {
-			visited.add(list[i].getName().replace(".html", ""));
-		}
-	}
-	
 	// create html file named as paramterized filename
 	// normalization took place: remove http/https protocol and redundant www.
 	private String createTitle(String filename) {
-		StringBuilder title = new StringBuilder(dest);
-		StringBuilder link = new StringBuilder();
+		StringBuilder address = new StringBuilder(dest);
+		// redundant http:// makes files' titles look much more dense
+		// remove it for simplicity
 		if(filename.contains("http://")) {
 			filename = filename.replace("http://", ""); 
-		}
-		else if(filename.contains("https://")) {
-			filename = filename.replace("https://", ""); 
 		}
 		else {
 			return "";
 		}
+		// if the URL contains www., remove it for simplicity
+		// URLs that lack www. do not cause prevent users from accessing them
 		if(filename.contains("www.")) {
 			filename = filename.replace("www.", "");
 		}
 		
-		link.append(filename);
-		if(filename.endsWith("/")) {
-			link.deleteCharAt(filename.length() - 1);
-		}
-		link.append(".html");
-		title.append(link.toString().replace("/", "-"));
+		// to distinguish local directory and web directory, replace / with -
+		address.append(filename.replace("/", "-"));
+		address.append(".html");
 		
-		return title.toString();
+		return address.toString();
+	}
+	
+	// reads in seed.txt and store URL to frontier
+	public void loadSeeds() {
+		String seedFile = "src/seed.txt";
+		
+		File seed = new File(seedFile);
+		Scanner s;
+		try {
+			s = new Scanner(seed);
+			
+			while(s.hasNextLine()) {
+				// upon creating LinkNode, anchors or references are removed
+				LinkNode node = new LinkNode(s.nextLine());
+				// if normalization succeeds, add node to the frontier
+				// otherwise(i.e. protocol is not http), ignores it and continue to the next seed
+				if(node.checkURL()) {
+					frontier.add(node);
+				}
+			}
+			s.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			System.out.println("File not found");
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Malformed URL is read");
+		}
 	}
 	
 	public void crawl() {
-		// add seed link to frontier, currently just manual
-		int numPages = 0;
-		String seedFile = "src/seed.txt";
-		
-		LinkNode seed = new LinkNode("http://alaska.edu");
-		frontier.add(seed);
-		numPages++;
+		// add seed link to frontier, currently just manually assigned
+		loadSeeds();
 		// run as long as frontier has some links to crawl
 		// for(int i = 0; i < 15; i++){
 		while(!frontier.isEmpty()) {
@@ -106,8 +118,6 @@ public class Crawler {
 				Elements links = doc.select("a[href]");
 				
 				String title = createTitle(curr.getLink());
-				// check title
-				System.out.println("Title: " + title);
 				
 				if(title.equals("")) {
 					// current link is not appropriate URL missing http:// or https://
@@ -118,30 +128,38 @@ public class Crawler {
 				writer.write(htmlText);
 				writer.close();
 				// to see what pages are visited
+				System.out.println("Title: " + title);
 				// System.out.println(curr.getLink());
 				// System.out.println(html.body());
+				
 				for(Element e: links) {
-					// if current page reaches max depth
-					//	or link is not .edu format
-					if(curr.getDepth() >= MAX_DEPTH || !e.absUrl("href").contains(".edu") || !e.absUrl("href").contains("https://")) {
+					// if current page reaches max depth, skip it
+					if(curr.getDepth() >= MAX_DEPTH) {
 						continue;
 					}
+					
 					LinkNode next = new LinkNode(e.absUrl("href"), curr.getDepth() + 1);
-					frontier.add(next);
+					if(next.checkURL()) {
+						frontier.add(next);
+					}
 				}
 			} catch (FileNotFoundException e) {
 				// TODO Auto-generated catch block
 				System.out.println(curr.getLink() + " cannot be found.");
-			} catch (IOException e) {
+			} catch (MalformedURLException e) {
 				// TODO Auto-generated catch block
-				System.out.println(curr.getLink() + " is not a proper URL.");
-				// e.printStackTrace();
+				// some links are not accessible due to not supported http protocol
+				System.out.println("Malformed URL is read");
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				System.out.println("Writing to file failed");
 			}
 		}
 	}
 	
 	// any data structure implementing Queue should work
 	private ArrayDeque<LinkNode> frontier;
+	private int numPages = 0;
 	private int MAX_DEPTH = 5;
 	private int MAX_NUM_PAGES = 100;
 	private final String dest = "src/pages/";
--- src/Crawler/LinkNode.java
+++ src/Crawler/LinkNode.java
@@ -1,24 +1,83 @@
 package Crawler;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class LinkNode {
-	public LinkNode(String link) {
-		this.link = link;
+	public LinkNode(String link) throws MalformedURLException {
+		removeRef(link);
+		
+		this.link = new URL(link);
 		depth = 1;
 	}
 	
-	public LinkNode(String link, int depth) {
-		this.link = link;
+	public LinkNode(String link, int depth) throws MalformedURLException {
+		removeRef(link);
+		
+		this.link = new URL(link);
 		this.depth = depth;
 	}
 	
-	public String getLink() {
+	// from the URL, this function decides whether URL is functional or not
+	public boolean checkURL() {
+		// if protocol is not http, return false
+		if(!checkProtocol()) {
+			return false;
+		}
+		// if the link is file rather than a link, return false
+		if(!checkFiles()) {
+			return false;
+		}
+		// if the link is pointing to non-edu sites, return false
+		if(!checkHost()) {
+			return false;
+		}
+		// if there exists an anchor, remove it
+		return true;
+	}
+	
+	// check if the link is in http protocol or not
+	private boolean checkProtocol() {
+		if(!link.getProtocol().equals("http")) {
+			return false;
+		}
+		return true;
+	}
+	
+	// check if the URL points to a file
+	// since files other than html will not be parsed, they will be ignored by crawlers
+	private boolean checkFiles() {
+		if(!link.getFile().equals("")) {
+			return false;
+		}
+		return true;
+	}
+	
+	// check if the URL links to .edu hosts
+	// if it does not contain .edu ignore it
+	private boolean checkHost() {
+		if(!link.getHost().contains(".edu")) {
+			return false;
+		}
+		return true;
+	}
+	
+	private String removeRef(String link){
+		if(link.contains("#")) {
+			return link.substring(0, link.indexOf("#"));
+		}
+		
 		return link;
+	}
+	
+	public String getLink() {
+		return link.toString();
 	}
 	
 	public int getDepth() {
 		return depth;
 	}
 	
-	private String link;
+	private URL link;
 	private int depth;
 }
